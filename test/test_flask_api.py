bc0913fc602ab1ffa2f28219753a4c5804dab78d
Pete Lealiiee Jr <petelealiieejr4@utexas.edu>
2022-05-10 00:00:45 -0500

    fixed api tests

--- test/test_flask_api.py
+++ test/test_flask_api.py
@@ -11,31 +11,31 @@ def test_route_perseverance():
     response = requests.get(f'{BASEROUTE}/perseverance')
     assert response.status_code == 200
     assert len(response.json())>0
-    assert response.json().keys() == ["type","properties","geometry"]
-    assert isinstance( response.json()["properties"]["sol"], float) == True
+    assert list( response.json()[0].keys() ) == ["type","properties","geometry"]
+    assert isinstance( response.json()[0]["properties"]["sol"], int) == True
 
 
 def test_route_sol():
     response = requests.get(f'{BASEROUTE}/perseverance/sol')
     assert response.status_code == 200
-    assert isinstance( response.text, float) == True
-    assert response.text[0:34] == "Most recent data waypoint is at Sol"
+    assert isinstance( str(response.text), str) == True
+    assert str(response.text[0:35]) == "Most recent data waypoint is at Sol"
 
 
 def test_route_ori():
     response = requests.get(f'{BASEROUTE}/perseverance/orientation')
     assert response.status_code == 200
     assert len(response.json())>0
-    assert response.json().keys() == ["sol","yaw_rad","pitch","roll"]
-    assert isinstance(response.json()["yaw_rad"], float) == True
+    assert list( response.json()[0].keys() ) == ["sol","yaw_rad","pitch","roll"]
+    assert isinstance(response.json()[0]["yaw_rad"], float) == True
 
 
 def test_route_pos():
     response = requests.get(f'{BASEROUTE}/perseverance/position')
     assert response.status_code == 200
     assert len(response.json())>0
-    assert response.json().keys() == ["sol","lon","lat"]
-    assert isinstance(response.json()["lon"], float) == True
+    assert list( response.json()[0].keys() ) == ["sol","lon","lat"]
+    assert isinstance(response.json()[0]["lon"], float) == True
 
 
 # ALL OTHER ROUTES ARE POST ROUTES FOR POST(ING) JOBS AND LOADERS 
